c93240f173f7e7a51a18f2392b89e9a9c83a3bce
melgacorm <rmmelgaco@gmail.com>
2024-03-28 13:39:02 -0300

    versao inicial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+<template>
+</template>
+
+<style scoped>
+.tab_menu {
+  margin: 5px;
+  display: flex;
+  justify-content: space-evenly;
+  border-bottom: 1px solid #52e7d6;
+}
+.tab_item {
+  padding: 10px;
+  color: #15295d;
+  flex: 1;
+  text-align: center;
+  font-size: larger;
+  font-weight: bolder;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+</style>
